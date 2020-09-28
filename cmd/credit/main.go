0000cf698933be36354c3f976ea24d2624873a81
Theodor Berkinblit <fb777@ya.ru>
2020-09-28 22:00:02 +0300

    implemented functions main and calculate

--- cmd/credit/main.go
+++ cmd/credit/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"gotask2-1/pkg/credit"
+)
 
+func main() {
+	var sumCredit int64 = 1000000_00
+	var creditRate int64 = 20
+	var periodCredit int64 = 3
+	fmt.Println(credit.Calculate(sumCredit, creditRate, periodCredit))
 }
--- pkg/credit/credit.go
+++ pkg/credit/credit.go
@@ -1,1 +1,15 @@
 package credit
+
+import (
+	"math"
+)
+
+func Calculate(sumCredit, creditRate, periodCredit int64) (monthlyPayment, overPayment, totalPayment int64) {
+	monthlyCreditRate := float64(creditRate) / 12 / 100
+	numberPeriods := float64(periodCredit) * 12
+	annuityRate := (monthlyCreditRate * math.Pow(1+monthlyCreditRate, numberPeriods)) / (math.Pow(1+monthlyCreditRate, numberPeriods) - 1)
+	monthlyPayment = int64(float64(sumCredit) * annuityRate)
+	totalPayment = monthlyPayment * int64(numberPeriods)
+	overPayment = totalPayment - sumCredit
+	return
+}
